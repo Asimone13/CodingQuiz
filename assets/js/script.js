7b868df8e3f99b993cc7532ee49313f005258dc3
Alec Simone <alecsimone2@gmail.com>
2022-10-22 10:37:42 -0400

    added more correct answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,33 @@ var questionsObject = { // Object that holds correct answers.
         4 : "Strings must be enclosed with:"
     }
 };
+
+var answersObject = { // Object that holds correct answers.
+    answers: { 
+        0 : {
+            0: "Strings",
+            1: "Boolean",
+            2: "Alerts",
+            3: "Numbers"},
+        1 : {
+            0: "Parentheses",
+            1: "Curly Brackets",
+            2: "Quotes",
+            3: "Square Brackets"},
+        2 : { // Button #3
+            0: "Javascript",
+            1: "Terminal/bash",
+            2: "For loops", 
+            3: "Console.log"},      
+        3 : { // Answer to question 5 --> Button #2
+            0: "Commas",
+            1: "Curly brackets",
+            2: "Quotes", 
+            3: "Parentheses"},      
+        4 : { // Button #4
+            0: "Number of strings",
+            1: "Other arrays",
+            2: "Booleans",
+            3: "All of the above"},  
+    }
+};
